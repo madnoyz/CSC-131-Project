948702134e6d882b4a480a93628a71869c16231f
Clyde <madnoyz@gmail.com>
2015-04-16 01:38:02 -0700

    added image

--- config/express.js
+++ config/express.js
@@ -1,6 +1,13 @@
 var express = require('express');
 module.exports = function() {
   var app = express();
+
+  // EJS Template Views
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
+
   require('../app/routes/index.server.routes.js')(app);
+  // Using images
+  app.use(express.static('./public'));
   return app;
 };
